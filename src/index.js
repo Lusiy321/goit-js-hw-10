63b2f253aae4e97c71160ed2ed757afdcc2d0142
Volodymyr Lysenko <lusiy321@gmail.com>
2022-11-20 12:24:55 +0200

    Finaly

--- src/index.js
+++ src/index.js
@@ -1,15 +1,87 @@
 import './css/styles.css';
-import restCountriesNode from 'rest-countries-node';
 import debounce from 'lodash.debounce';
-import Notiflix from 'notiflix';
-const DEBOUNCE_DELAY = 300;
+import { fetchCountries } from './fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const app = restCountriesNode();
+const input = document.querySelector('#search-box');
+const countList = document.querySelector('.country-list');
+const countInfo = document.querySelector('.country-info');
+input.addEventListener('input', debounce(onSearch, 300));
 
-// const obj = {
-//   name: {
-//     common: 'Ukraine',
-//     official: 'Ukraine',
-//     nativeName: { ukr: { official: 'Україна', common: 'Україна' } },
-//   },
-// };
+function onSearch(e) {
+  e.preventDefault();
+  const val = input.value.trim();
+  if (!val) {
+    Notify.failure('Please enter correct name', {
+      opacity: 0.5,
+      position: 'center-top',
+      timeout: 2000,
+      backOverlay: true,
+      cssAnimationDuration: 300,
+      backOverlayColor: 'rgb(255,255,255)',
+      cssAnimationStyle: 'zoom',
+    });
+    countList.innerHTML = null;
+    countInfo.innerHTML = null;
+    return;
+  }
+  fetchCountries(val)
+    .then(data => createMarkup(data))
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name', {
+        opacity: 0.5,
+        position: 'center-top',
+        timeout: 2000,
+        backOverlay: true,
+        cssAnimationDuration: 300,
+        backOverlayColor: 'rgb(255,255,255)',
+        cssAnimationStyle: 'zoom',
+      });
+      countList.innerHTML = null;
+      countInfo.innerHTML = null;
+      return;
+    });
+}
+
+function createMarkup(arr) {
+  if (arr.length > 10) {
+    Notify.failure(
+      'Too many matches found. Please enter a more specific name.',
+      {
+        opacity: 0.5,
+        position: 'center-top',
+        timeout: 2000,
+        backOverlay: true,
+        cssAnimationDuration: 300,
+        backOverlayColor: 'rgb(255,255,255)',
+        cssAnimationStyle: 'zoom',
+      }
+    );
+    countList.innerHTML = null;
+    countInfo.innerHTML = null;
+  } else {
+    const markup = arr
+      .map(
+        item =>
+          `<li><img src="${item.flags.png}" alt="${item.name}"><h2>${item.name}</h2></li>`
+      )
+      .join('');
+    countList.innerHTML = markup;
+  }
+
+  if (arr.length <= 1) {
+    const descr = arr
+      .map(
+        item =>
+          `<ul>
+  <li>Capital: ${item.capital}</li>
+  <li>Population: ${item.population}</li>
+  <li>Languages: ${item.languages[0].name}</li>
+</ul>`
+      )
+      .join('');
+    countInfo.innerHTML = descr;
+  } else {
+    countInfo.innerHTML = null;
+  }
+}
